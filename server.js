fba2182e4b32008700fd1310e2e4ece82bac698c
Cody Yuill <codyyuill@hotmail.com>
2021-02-02 15:14:35 -0500

    added html files, css files, js files, gitignore, db json. setup express server with most needed calls

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const db = require("./db/db.json")
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res){
+    res.json(db)
+});
+
+app.post("/api/notes", function(req, res){
+    var newNote = req.body;
+    var notes = JSON.parse(fs.readFileSync("./db/db.json", 'utf8', function(err, data){
+        if(err) 
+            throw err;
+        else 
+            console.log("Notes Read succsefully: " + data);
+    }));
+
+    notes.push(newNote);
+    console.log(notes);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), function(err){
+        if(err) 
+            throw err;
+        else 
+            res.send("Note Succesfully added!")
+    });
+    // res.json(db)
+});
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
