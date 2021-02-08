4c0fa0d8afdbc9a55f5ae6d16f8aa1eb0757cb4a
Cody Yuill <codyyuill@hotmail.com>
2021-02-08 18:27:35 -0500

    new server.json

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/notes", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res){
-    res.json(db)
+    res.json(db);
 });
 
 app.post("/api/notes", function(req, res){
@@ -31,7 +31,7 @@ app.post("/api/notes", function(req, res){
     //     else 
     //         console.log("Notes Read succsefully: " + data);
     // }));
-    var notes = []
+    var notes = [];
     fs.readFile(path.join(__dirname, "/db/db.json"), 'utf8', function(err, data){
         if(err) 
             throw err;
@@ -51,7 +51,7 @@ app.post("/api/notes", function(req, res){
                 if(err) 
                     throw err;
                 else 
-                    res.send("Note Succesfully added!")
+                    res.send("Note Succesfully added!");
             });
         }
     });
@@ -89,3 +89,94 @@ app.get("*", function(req, res) {
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+// const express = require("express");
+// const path = require("path");
+// const fs = require("fs");
+
+// const app = express();
+// const PORT = process.env.PORT || 8080;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// const db = require(path.join(__dirname, "/db/db.json"));
+// console.log(db);
+
+
+// app.get("/notes", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/public/notes.html"));
+// });
+
+// app.get("/api/notes", function(req, res){
+//     res.json(db);
+// });
+
+// app.post("/api/notes", function(req, res){
+//     var newNote = req.body;
+    
+
+//     // var notes = JSON.parse(fs.readFileSync("./db/db.json", 'utf8', function(err, data){
+//     //     if(err) 
+//     //         throw err;
+//     //     else 
+//     //         console.log("Notes Read succsefully: " + data);
+//     // }));
+//     var notes = [];
+//     fs.readFile(path.join(__dirname, "/db/db.json"), 'utf8', function(err, data){
+//         if(err) 
+//             throw err;
+//         else{
+//             notes = JSON.parse(data);
+//             if(notes.length === 0)
+//             {
+//                 newNote.id = 0;
+//             }
+//             else{
+//                 newNote.id = notes[notes.length - 1].id + 1;
+//             }
+//             notes.push(newNote);
+//             console.log(notes);
+        
+//             fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notes), function(err){
+//                 if(err) 
+//                     throw err;
+//                 else 
+//                     res.send("Note Succesfully added!");
+//             });
+//         }
+//     });
+    
+// });
+
+// app.delete("/api/notes/:id", function(req, res){
+//     var id = req.params.id;
+
+//     var notes = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"), 'utf8', function(err, data){
+        
+//         if(err) 
+//             throw err;
+//         else 
+//             console.log("Notes Read succsefully: " + data);
+//     }));
+    
+
+//     var newNotes = notes.filter(note => note.id != id);
+
+//     fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newNotes), function(err){
+        
+//         if(err) 
+//             throw err;
+//         else 
+//             res.send("File deleted succesfully");
+//     });
+// });
+
+// app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/public/index.html"));
+// });
+
+
+// app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+// });
